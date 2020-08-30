cb50a97eed28c9ebd9c6c56ac3f5564c381eb24b
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-30 18:05:56 +0530

    Hospital api done

---
+++ model/patient.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// create patient schema
+const pSchema = new mongoose.Schema({
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String
+    },
+    report:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Report'
+    }]
+}, {
+    timestamps: true
+});
+
+const Patient = mongoose.model('Patient', pSchema);
+
+// export patient
+module.exports = Patient;
